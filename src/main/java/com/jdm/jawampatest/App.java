d0eea864e32c1dc492857207465709ca2118eb3d
Johan 't Hart <johanthart@gmail.com>
2017-10-06 21:37:07 +0200

    Add jawampa dependencies

--- src/main/java/com/jdm/jawampatest/App.java
+++ src/main/java/com/jdm/jawampatest/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        for (int i=0; i<100; ++i)
+            System.out.print(String.format("%d,", i));
+        System.out.println(100);
     }
 }
