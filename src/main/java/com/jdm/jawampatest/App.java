eec2020b4eccf423691d6f23d2ea545bd671bf81
Johan 't Hart <johan@jdm.nl>
2017-10-07 01:14:02 +0200

    Beginnings of a wamp proxy

--- src/main/java/com/jdm/jawampatest/App.java
+++ src/main/java/com/jdm/jawampatest/App.java
@@ -1,12 +1,85 @@
 package com.jdm.jawampatest;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import rx.Observable;
+import rx.Subscription;
 import ws.wamp.jawampa.WampClient;
 import ws.wamp.jawampa.WampClientBuilder;
 import ws.wamp.jawampa.WampError;
 import ws.wamp.jawampa.connection.IWampConnectorProvider;
 import ws.wamp.jawampa.transport.netty.NettyWampClientConnectorProvider;
-import ws.wamp.jawampa.WampClient.ConnectingState;
 
+interface IJWampProxy
+{
+    Object call(String name, Object... args);
+}
+
+class JWamp
+{
+    static class JawampaProxy implements IJWampProxy
+    {
+        private WampClient wamp;
+
+        public JawampaProxy(WampClient wamp)
+        {
+            this.wamp = wamp;
+        }
+
+        public Object call(String name, Object... args)
+        {
+            return wamp.call(name, args);
+        }
+    }
+
+    public static Observable<IJWampProxy> fromJawampa(String uri, String realm)
+    {
+        return Observable.create(observer -> {
+            IWampConnectorProvider connectorProvider = new NettyWampClientConnectorProvider();
+            WampClientBuilder builder = new WampClientBuilder();
+            builder.withConnectorProvider(connectorProvider)
+                    .withUri(uri)
+                    .withRealm(realm);
+            try {
+                final WampClient client = builder.build();
+                AtomicInteger connectionState = new AtomicInteger(0);
+                observer.add(client.statusChanged().subscribe(
+                    state -> {
+                        if (state instanceof WampClient.ConnectingState) {
+                            connectionState.set(1);
+                            return;
+                        }
+                        else if (state instanceof WampClient.ConnectedState) {
+                            connectionState.set(2);
+                            observer.onNext(new JWamp.JawampaProxy(client));
+                        }
+                        else if (state instanceof WampClient.DisconnectedState) {
+                            switch (connectionState.get()) {
+                                case 0: return;
+                                case 1: observer.onError(((WampClient.DisconnectedState)state).disconnectReason()); return;
+                                case 2: observer.onCompleted(); return;
+                            }
+                        }
+                    }
+                ));
+
+                client.open();
+                
+                observer.add(new Subscription() {
+                    boolean isSubscribed = true;
+                    // This is called when the observable is unsubscribed
+                    public void unsubscribe() {
+                        isSubscribed = false;
+                        client.close();
+                    }
+                    public boolean isUnsubscribed() { return !isSubscribed; }
+                });
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
+    }
+}
 /**
  * Hello world!
  *
@@ -19,30 +92,14 @@ public class App
         System.out.println();
     }
 
-    public WampClient createWampClient() throws Exception
-    {
-        IWampConnectorProvider connectorProvider = new NettyWampClientConnectorProvider();
-        WampClientBuilder builder = new WampClientBuilder();
-        builder.withConnectorProvider(connectorProvider)
-                .withUri("ws://pcs02.otap.local:9001/wamp")
-                .withRealm("realm1");
-        // Create a client through the builder. This will not immediatly start
-        // a connection attempt
-        return builder.build();
-    }
-
     public void run() throws Exception
     {
-        final WampClient client = createWampClient();
-        client.open();
-        client.statusChanged()
-            //.subscribeOn(Schedulers.newThread())
-            //.observeOn(Schedulers.io())
-            .takeUntil(v -> !(v instanceof ConnectingState))
+        JWamp.fromJawampa("ws://pcs02.otap.local:9001/wamp", "realm1")
+            .map($ -> "Success!")
+            .onErrorResumeNext($ -> Observable.just("Failed!"))
+            .take(1)
             .toBlocking()
-            .forEach(s -> log(1, s.toString()));
-
-        client.close().toBlocking().last();
+            .forEach(s -> log(1, s));
     }
 
     public static void main( String[] args )
@@ -50,6 +107,8 @@ public class App
         log(1, "Program started!" );
         try {
             new App().run();
+            log(0, "Wait a little while...");
+            Thread.sleep(500);
         } catch (WampError e) {
             // Catch exceptions that will be thrown in case of invalid configuration
             System.out.println(e);
