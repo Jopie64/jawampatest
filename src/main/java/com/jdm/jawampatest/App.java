a49a2864a59ca0a3bddd56173aedf0515cb5a9d3
Johan 't Hart <johan@jdm.nl>
2017-10-06 23:42:59 +0200

    Create a connection to the router

--- src/main/java/com/jdm/jawampatest/App.java
+++ src/main/java/com/jdm/jawampatest/App.java
@@ -1,6 +1,9 @@
 package com.jdm.jawampatest;
 
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import rx.schedulers.Schedulers;
 import ws.wamp.jawampa.WampClient;
 import ws.wamp.jawampa.WampClientBuilder;
 import ws.wamp.jawampa.WampError;
@@ -12,14 +15,20 @@ import ws.wamp.jawampa.transport.netty.NettyWampConnectionConfig;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    public static void log(int sev, String msg)
+    {
+        System.out.format("%d {%04X} %s", sev, Thread.currentThread().getId(), msg);
+        System.out.println();
+    }
+
     public WampClient createWampClient() throws Exception
     {
         IWampConnectorProvider connectorProvider = new NettyWampClientConnectorProvider();
         WampClientBuilder builder = new WampClientBuilder();
         builder.withConnectorProvider(connectorProvider)
-                .withUri("ws://pcs01.otap.local:9001/wamp")
+                .withUri("ws://pcs02.otap.local:9001/wamp")
                 .withRealm("realm1");
         // Create a client through the builder. This will not immediatly start
         // a connection attempt
@@ -29,12 +38,17 @@ public class App
     public void run() throws Exception
     {
         final WampClient client = createWampClient();
-        client.statusChanged().subscribe(s -> System.out.println(s.toString()));
+        client.open();
+        client.statusChanged()
+            //.subscribeOn(Schedulers.newThread())
+            //.observeOn(Schedulers.io())
+            .toBlocking()
+            .forEach(s -> log(1, s.toString()));
     }
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        log(1, "Program started!" );
         try {
             new App().run();
         } catch (WampError e) {
@@ -45,6 +59,6 @@ public class App
 			// TODO Auto-generated catch block
 			e.printStackTrace();
         }
-        System.out.println("End");
+        log(1, "Program ended." );
     }
 }
