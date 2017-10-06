5f5dcaab658c93d95ada577b0f2e6450be3a6114
Johan 't Hart <johanthart@gmail.com>
2017-10-06 20:20:33 +0200

    Initial

---
+++ src/main/java/com/jdm/jawampatest/App.java
@@ -0,0 +1,13 @@
+package com.jdm.jawampatest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
