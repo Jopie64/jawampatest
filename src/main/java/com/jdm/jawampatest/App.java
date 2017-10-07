1bc9765d8f36cb98526d4c70f0672c2d1ae1147e
Johan 't Hart <johan@jdm.nl>
2017-10-08 00:47:11 +0200

    Experiment with proxies
    Failed... :(

--- src/main/java/com/jdm/jawampatest/App.java
+++ src/main/java/com/jdm/jawampatest/App.java
@@ -2,7 +2,12 @@ package com.jdm.jawampatest;
 
 import com.jdm.jwamp.IJWampProxy;
 import com.jdm.jwamp.JWampFactory;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import rx.Observable;
+import rx.Subscription;
+import rx.schedulers.Schedulers;
 
 /**
  * Hello world!
@@ -33,16 +38,40 @@ public class App
             .forEach(s -> log(1, s)); // forEach() called only once due to take(1)
     }
 
-    public void run()
+    public void run() throws IOException, InterruptedException
     {
-        wamp
+        Observable<IJWampProxy> wamp = this.wamp
+            .retryWhen(e -> Observable.timer(3, TimeUnit.SECONDS))
+            .repeatWhen(e -> Observable.timer(3, TimeUnit.SECONDS))
+            .replay(1).refCount();
+        Observable<IJWampProxy> toolbarRoot = wamp
             .map(p -> p.makeProxy("com.peterconnects.toolbar."))
+            .replay(1).refCount();
+        Observable<IJWampProxy> toolbar = toolbarRoot
             .switchMap(p -> p.call("Login"))
-            .map(v -> "Success! " + v.asInt())
-            .onErrorResumeNext(e -> Observable.just("Failed! " + e.toString()))
-            .take(1)
-            .toBlocking() // toBlocking() is bad! Quick and dirty synchronous way to wait for success or failed.
-            .forEach(s -> log(1, s)); // forEach() called only once due to take(1)
+            .switchMap(v -> toolbarRoot.map(p -> p.makeProxy(String.format("%s.", v.asText()))))
+            .replay(1).refCount();
+        Observable<String> initialized = toolbar
+            .switchMap(p -> p.call("Initialize"))
+            .map(v -> String.format("Init %sOK", v.asBoolean() ? "": "NOT "));
+
+        
+        
+        Observable<String> log$ = Observable.merge(
+            wamp.map(p -> "Connected!"),
+            toolbar.map(p -> "Logged in!"),
+            initialized);
+
+        //log$ = wamp.map(v -> "Now then?");// Observable.just("Do I work?");
+
+        Subscription sub = log$
+            .subscribe(s -> log(1, s));
+/*        while(true) {
+            if ((char)System.in.read() == 'q')
+                break;
+        }
+*/      Thread.sleep(300000);
+        sub.unsubscribe();
     }
 
     public static void main( String[] args )
@@ -50,8 +79,8 @@ public class App
         log(1, "Program started!" );
         try {
             App app = new App("ws://pcs02.otap.local:9001/wamp", "realm1");
-            app.test();
-            //app.run();
+            // app.test();
+            app.run();
             log(0, "Wait a little while...");
             Thread.sleep(500);
         } catch (Exception e) {
