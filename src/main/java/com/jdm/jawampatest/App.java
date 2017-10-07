22197258b5fe305613af34daf2633e274b0f9270
Johan 't Hart <johan@jdm.nl>
2017-10-07 22:40:05 +0200

    SubProxies are possible now

--- src/main/java/com/jdm/jawampatest/App.java
+++ src/main/java/com/jdm/jawampatest/App.java
@@ -1,5 +1,6 @@
 package com.jdm.jawampatest;
 
+import com.jdm.jwamp.IJWampProxy;
 import com.jdm.jwamp.JWampFactory;
 import rx.Observable;
 
@@ -15,10 +16,28 @@ public class App
         System.out.println();
     }
 
+    private Observable<IJWampProxy> wamp;
+
+    App(String uri, String realm)
+    {
+        wamp = JWampFactory.jawampa(uri, realm);
+    }
+
+    public void test()
+    {
+        wamp
+            .map(v -> "Success!")
+            .onErrorResumeNext(e -> Observable.just("Failed! " + e.toString()))
+            .take(1)
+            .toBlocking() // toBlocking() is bad! Quick and dirty synchronous way to wait for success or failed.
+            .forEach(s -> log(1, s)); // forEach() called only once due to take(1)
+    }
+
     public void run()
     {
-        JWampFactory.jawampa("ws://pcs02.otap.local:9001/wamp", "realm1")
-            .switchMap(p -> p.call("com.peterconnects.toolbar.Login"))
+        wamp
+            .map(p -> p.makeProxy("com.peterconnects.toolbar."))
+            .switchMap(p -> p.call("Login"))
             .map(v -> "Success! " + v.asInt())
             .onErrorResumeNext(e -> Observable.just("Failed! " + e.toString()))
             .take(1)
@@ -30,7 +49,9 @@ public class App
     {
         log(1, "Program started!" );
         try {
-            new App().run();
+            App app = new App("ws://pcs02.otap.local:9001/wamp", "realm1");
+            app.test();
+            //app.run();
             log(0, "Wait a little while...");
             Thread.sleep(500);
         } catch (Exception e) {
--- src/main/java/com/jdm/jwamp/IJWampProxy.java
+++ src/main/java/com/jdm/jwamp/IJWampProxy.java
@@ -3,8 +3,39 @@ package com.jdm.jwamp;
 import com.fasterxml.jackson.databind.JsonNode;
 import rx.Observable;
 
+class SubProxy implements IJWampProxy
+{
+    String uriBase;
+    IJWampProxy proxyBase;
+
+    SubProxy(String uriBase, IJWampProxy proxyBase)
+    {
+        this.uriBase = uriBase;
+        this.proxyBase = proxyBase;
+    }
+
+    String formatUri(String uri)
+    {
+        return uriBase + uri;
+    }
+
+    public Observable<JsonNode> call(String name, Object... args)
+    {
+        return proxyBase.call(formatUri(name), args);
+    }
+
+    public Observable<JsonNode> subscribe(String name)
+    {
+        return proxyBase.subscribe(formatUri(name));
+    }
+}
+
 public interface IJWampProxy
 {
     Observable<JsonNode> call(String name, Object... args);
     Observable<JsonNode> subscribe(String name);
+    default IJWampProxy makeProxy(String uriBase)
+    {
+        return new SubProxy(uriBase, this);
+    }
 }
